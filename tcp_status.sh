6769eedf0ad52ac925b0ea3403553cfa85ae47f6
root <root@shzll.top>
2019-08-22 14:27:06 +0800

    当前tcp状态

--- tcp_status.sh
+++ tcp_status.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #统计当前tcp状态和连接数
 netstat -ant|awk '{s[$NF]++}END{for(k in s)print k,s[k]}' >/tmp/tcpstatus
-netstat -t|awk -F '[:  ]++' 'NR>2 {print $6}' >/tmp/tcpstatus_ip
+netstat -tn|awk -F '[:  ]++' 'NR>2 {print $6}' >/tmp/tcpstatus_ip
 echo -e "\033[32m1.当前tcp连接状态\n2.当前连接到本机的ip\033[0m"
 read -p "输入你想查看的: " a
 
